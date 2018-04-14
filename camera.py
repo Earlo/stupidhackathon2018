bd1b239294d065422479ee79e8fc26b3f8801bc6
Joonatan Partanen <joonatan.partanen@aalto.fi>
2018-04-14 08:19:49 +0300

    Kaikki push

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class VideoCamera1(object):
@@ -22,6 +23,15 @@ class VideoCamera1(object):
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
+    def get_jpg(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg
+
+
 class VideoCamera2(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
@@ -42,3 +52,41 @@ class VideoCamera2(object):
         # video stream.
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
+
+    def get_jpg(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg
+
+
+class JoinedImage(object):
+    def __init__(self, c1, c2):
+        self.cam1 = c1
+        self.cam2 = c2
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        img1 = self.cam1.get_jpg()
+        img2 = self.cam2.get_jpg()
+        frame = np.hstack((img1, img2))
+        frame = cv2.imencode('.jpg', frame).tobytes()
+        return (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+class ImageGetter:
+    def __init__(self, cam):
+        self.cam = cam
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        frame = self.cam.get_frame()
+        return (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, Response
-from camera import VideoCamera1, VideoCamera2
+from camera import VideoCamera1, VideoCamera2, ImageGetter, JoinedImage
 from multiprocessing import Process
 import time
+import numpy as np
 
 app = Flask(__name__)
 left_frames = ""
@@ -12,7 +13,16 @@ def index():
     return render_template('index.html')
 
 
+def timed_left(t):
+    gen1(VideoCamera1)
+    time.sleep(t)
+
+def timed_right(t):
+    gen2(VideoCamera2)
+    time.sleep(t)
+
 def gen1(camera):
+    global left_frames
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -20,25 +30,33 @@ def gen1(camera):
         time.sleep(0)
 
 def gen2(camera):
+    global right_frames
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         time.sleep(0)
 
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(JoinedImage(VideoCamera1(), VideoCamera2()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 @app.route('/left_video_feed')
 def left_video_feed():
-    async_response = Process(target=gen1, args=(VideoCamera1(),)).start()
-    return Response(async_response.get(),
+    #async_response = Process(target=gen1, args=(VideoCamera1(),)).start()
+    return Response(ImageGetter(VideoCamera1()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/right_video_feed')
 def right_video_feed():
-    async_response = Process(target=gen2, args=(VideoCamera2(),)).start()
-    return Response(async_response.get(),
+    #async_response = Process(target=gen2, args=(VideoCamera2(),)).start()
+    return Response(ImageGetter(VideoCamera2()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=8080, debug=True)
