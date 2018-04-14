e63a71a11518d24763b73558292a05fd57f629c5
Nicholas Saarela <nicholas.saarela@gmail.com>
2018-04-14 03:00:30 +0300

    tried multiple cameras

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-from camera import VideoCamera
+from camera import VideoCamera1, VideoCamera2
 
 app = Flask(__name__)
 
@@ -13,9 +13,14 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
+@app.route('/left_video_feed')
+def left_video_feed():
+    return Response(gen(VideoCamera1()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/right_video_feed')
+def right_video_feed():
+    return Response(gen(VideoCamera2()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
